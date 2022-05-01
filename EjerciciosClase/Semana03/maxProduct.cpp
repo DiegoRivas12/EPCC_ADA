eb17d4e496232be7fcf059d66b3671bd953958c6
Diego Rivas <drivas@unsa.edu.pe>
2022-05-01 18:02:36 -0500

    Agregando comentarios a maxProduct.cpp

--- EjerciciosClase/Semana03/maxProduct.cpp
+++ EjerciciosClase/Semana03/maxProduct.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+/*
+PRODUCTO MAXIMO DE DOS NUMEROS
+Dada un secuencia de n numeros no negativos y mayores que cero.
+El objetivo es encontrar el mayor numero que se pueda obtener
+multiplicando dos numerosa de esta secuencia
+*/
 int maxProduct(int*,int);
 
 
@@ -12,8 +18,8 @@ int main(){
 
 int maxProduct(int*A,int lon){
     int resultado=A[0];
-    for(int i=0;i<lon;i++){
-        for(int j=i+1;j<lon-1;j++){
+    for(int i=0;i<lon;i++){//Recorre todo el array
+        for(int j=i+1;j<lon-1;j++){//No es necesario analizar los numeros mas a la izquierd aa medida que avanza i
             if(A[i]*A[j]>resultado){
                 resultado=A[i]*A[j];
             }

0240fb5df77d989e3025630bfb0ca6fe56043f7e
Diego Rivas <drivas@unsa.edu.pe>
2022-05-01 17:51:17 -0500

    Resolviendo con dos for

--- EjerciciosClase/Semana03/maxProduct.cpp
+++ EjerciciosClase/Semana03/maxProduct.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+using namespace std;
+int maxProduct(int*,int);
+
 
 int main(){
-
-
-
+    int lon=10;
+    int valores[lon]={8,1,6,3,4,10,3,9,14,2};
+    cout<<maxProduct(valores,lon)<<endl;
     return 0;
+}
+
+int maxProduct(int*A,int lon){
+    int resultado=A[0];
+    for(int i=0;i<lon;i++){
+        for(int j=i+1;j<lon-1;j++){
+            if(A[i]*A[j]>resultado){
+                resultado=A[i]*A[j];
+            }
+        }
+    }
+    return resultado;
 }

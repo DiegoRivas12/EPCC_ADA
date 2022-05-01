c7ba3e8c637449067d70a11a65b79d9ce8ae8824
Diego Rivas <drivas@unsa.edu.pe>
2022-05-01 18:27:23 -0500

    Empezando desde el final maxProduct.cpp

--- EjerciciosClase/Semana03/maxProduct.cpp
+++ EjerciciosClase/Semana03/maxProduct.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 /*
 PRODUCTO MAXIMO DE DOS NUMEROS
@@ -6,20 +7,20 @@ Dada un secuencia de n numeros no negativos y mayores que cero.
 El objetivo es encontrar el mayor numero que se pueda obtener
 multiplicando dos numerosa de esta secuencia
 */
-int maxProduct(int*,int);
+int maxProduct(vector<int>,int);
 
 
 int main(){
-    int lon=10;
-    int valores[lon]={8,1,6,3,4,10,3,9,14,2};
+    vector<int> valores{8,1,6,3,4,10,3,9,2,14};
+    int lon=valores.size();
     cout<<maxProduct(valores,lon)<<endl;
     return 0;
 }
 
-int maxProduct(int*A,int lon){
-    int resultado=A[0];
-    for(int i=0;i<lon;i++){//Recorre todo el array
-        for(int j=i+1;j<lon-1;j++){//No es necesario analizar los numeros mas a la izquierd aa medida que avanza i
+int maxProduct(vector<int>A,int lon){
+    int resultado=A[lon-1];
+    for(int i=lon-1;i>=0;i--){//Recorre todo el array
+        for(int j=i-1;j>=0;j--){//No es necesario analizar los numeros mas a la izquierd aa medida que avanza
             if(A[i]*A[j]>resultado){
                 resultado=A[i]*A[j];
             }
@@ -27,3 +28,28 @@ int maxProduct(int*A,int lon){
     }
     return resultado;
 }
+/*int maxProduct(int*A,int lon){
+    int resultado=A[lon-1];
+    for(int i=lon-1;i>=0;i--){//Recorre todo el array
+        for(int j=i-1;j>=0;j--){//No es necesario analizar los numeros mas a la izquierd aa medida que avanza
+            if(A[i]*A[j]>resultado){
+                resultado=A[i]*A[j];
+            }
+        }
+    }
+    return resultado;
+}*/
+
+/*
+int maxProduct(int*A,int lon){
+    int resultado=A[0];
+    for(int i=0;i<lon;i++){//Recorre todo el array
+        for(int j=i+1;j<lon;j++){//No es necesario analizar los numeros mas a la izquierd aa medida que avanza
+            if(A[i]*A[j]>resultado){
+                resultado=A[i]*A[j];
+            }
+        }
+    }
+    return resultado;
+}
+*/

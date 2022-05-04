09bac581f1c6282733175c5cd5c791b3157b6012
Diego Rivas <drivas@unsa.edu.pe>
2022-05-04 16:17:17 -0500

    Ordenamiento por insercion a

--- EjerciciosClase/Semana03/insertionSort.cpp
+++ EjerciciosClase/Semana03/insertionSort.cpp
@@ -26,7 +26,7 @@ void insertion_sort(int* A,int lon){
     for(int j=1;j<lon;j++){
         key=A[j];
         i=j-1;
-        while(i>=0 && A[i]>key){
+        while(i>=0 && A[i]<key){
             A[i+1]=A[i];
             i=i-1;
         }
